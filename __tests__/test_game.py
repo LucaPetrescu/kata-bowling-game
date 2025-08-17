9056892359e4c08dbe0018ce11e77cf93d0d5055
Luca Petrescu <luca.petrescu@inthepocket.com>
2025-08-17 23:37:25 +0300

    feat: add first test

--- __tests__/test_game.py
+++ __tests__/test_game.py
@@ -6,6 +6,10 @@ def roll_many(game, rolls, pins):
     for _ in range(rolls):
         game.roll(pins)
 
+def roll_spare(game):
+    game.roll(5)
+    game.roll(5)
+
 def test_game_initialization(game):
     assert game is not None
     assert isinstance(game, Game)
@@ -13,11 +17,5 @@ def test_game_initialization(game):
 def test_gutter_game(game):
     roll_many(game, 20, 0)
     assert game.score() == 0;
-    
 
-def test_all_ones(game):
-    roll_many(game, 20, 1)
-    assert game.score() == 20
 
-def test_one_spare(game):
-    game.roll(5)
